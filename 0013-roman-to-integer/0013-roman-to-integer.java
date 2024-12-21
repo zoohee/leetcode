5fdb96934f288f91ad3f76898fcf98089e6519e1
JUHEE LEE <48899055+zoohee@users.noreply.github.com>
2024-12-21 23:47:52 +0900

    Time: 5 ms (62.48%), Space: 45.2 MB (8.42%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> romanMap = Map.of(
+                'I', 1,
+                'V', 5,
+                'X', 10,
+                'L', 50,
+                'C', 100,
+                'D', 500,
+                'M', 1000
+        );
+
+        int answer = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char currentChar = s.charAt(i);
+            int currentValue = romanMap.get(currentChar);
+
+            if (currentValue < prevValue) {
+                answer -= currentValue;
+            } else {
+                answer += currentValue;
+            }
+
+            prevValue = currentValue;
+        }
+
+        return answer;
+    }
+}
